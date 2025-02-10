8b6ac8054c94927ce95f084fc7cc4daa1e4628ed
thomasthomsen16 <35918236+thomasthomsen16@users.noreply.github.com>
2025-02-10 12:01:15 +0100

    Style sliders

--- main.js
+++ main.js
@@ -11,27 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
             renderCharts(sampledData);
         })
         .catch(error => console.error("Error loading CSV data: ", error));
-
-        // Grab the value of the sliders and set the buttons to active when slider = 0 
-        const ySlider = document.getElementById('yCollapseSlider');
-        const yButton = document.getElementById('y-button');
-        const xSlider = document.getElementById('xCollapseSlider');
-        const xButton = document.getElementById('x-button');
-    
-        ySlider.addEventListener('input', function() {
-            if (ySlider.value == 0) {
-                yButton.disabled = false;
-            } else {
-                yButton.disabled = true;
-            }
-        });
-        xSlider.addEventListener('input', function() {
-            if (xSlider.value == 0) {
-                xButton.disabled = false;
-            } else {
-                xButton.disabled = true;
-            }
-        });
 });
 
 function renderCharts(sampledData) {
@@ -108,6 +87,27 @@ yCollapseSlider.addEventListener("input", function(e) {
       interactiveChartView.signal("collapseYSignal", newValue).runAsync();
     }
   });
+
+// Grab the value of the sliders and set the buttons to active when slider = 0 
+const ySlider = document.getElementById('yCollapseSlider');
+const yButton = document.getElementById('y-button');
+const xSlider = document.getElementById('xCollapseSlider');
+const xButton = document.getElementById('x-button');
+
+yCollapseSlider.addEventListener('input', function () {
+    if (yCollapseSlider.value == 0) {
+        yButton.disabled = false;
+    } else {
+        yButton.disabled = true;
+    }
+});
+xCollapseSlider.addEventListener('input', function () {
+    if (xCollapseSlider.value == 0) {
+        xButton.disabled = false;
+    } else {
+        xButton.disabled = true;
+    }
+});
 
 // Function to parse CSV data into an array of objects
 function parseCSV(csvData) {
